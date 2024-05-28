af094f576776646876bd8791e15da1c91cc6268a
marvelayanbisi <adebisimarvellous947@gmail.com>
2024-05-28 22:44:55 +0100

    solution 1

---
+++ class.py
@@ -0,0 +1,9 @@
+def main():
+    answer = input("What is the answer to the great question of life, the universe, and everything? ")
+    if 
+    answer.strip() == "42":
+    print("Yes")
+    else:
+    print("No")
+    if __name__ == "__main__":
+    main()
